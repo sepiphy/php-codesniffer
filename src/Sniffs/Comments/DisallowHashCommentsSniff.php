f31ffef886a7c1db8661d4bc855b1e110be90d52
Nguyen Xuan Quynh <nguyenxuanquynh2210vghy@gmail.com>
2018-08-16 09:24:04 +0700

    Disallow hash comments

---
+++ src/Sniffs/Comments/DisallowHashCommentsSniff.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace XuanQuynh\CodeSniffer\Sniffs\Comments;
+
+use PHP_CodeSniffer\Sniffs\Sniff;
+use PHP_CodeSniffer\Files\File;
+
+class DisallowHashCommentsSniff implements Sniff
+{
+    /**
+     * Returns the token types that this sniff is interested in.
+     *
+     * @return array
+     */
+    public function register()
+    {
+        return [T_COMMENT];
+
+    }
+
+
+    /**
+     * Processes this sniff, when one of its tokens is encountered.
+     *
+     * @param  \PHP_CodeSniffer\Files\File  $phpcsFile  The current file being checked.
+     * @param  int                          $stackPtr   The position of the current token in the stack passed in $tokens.
+     * @return void
+     */
+    public function process(File $phpcsFile, $stackPtr)
+    {
+        $tokens = $phpcsFile->getTokens();
+        if ($tokens[$stackPtr]['content']{0} === '#') {
+            $error = 'Hash comments are prohibited; found "%s"';
+            $data  = array(trim($tokens[$stackPtr]['content']));
+            $phpcsFile->addError($error, $stackPtr, 'Found', $data);
+        }
+
+    }
+}
