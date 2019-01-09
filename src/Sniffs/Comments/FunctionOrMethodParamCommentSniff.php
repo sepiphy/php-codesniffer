4bb54e62b61efde0785d9988da9c5b28e5c70a73
Nguyễn Xuân Quỳnh <nguyenxuanquynh2210vghy@gmail.com>
2019-01-09 08:25:07 +0700

    feat: Rename notation classes BREAKING CHANGE: could not use *NotationSniff anymore

--- src/Sniffs/Comments/FunctionOrMethodParamCommentSniff.php
+++ src/Sniffs/Comments/FunctionOrMethodParamCommentSniff.php
@@ -5,7 +5,7 @@ namespace XuanQuynh\CodeSniffer\Sniffs\Comments;
 use PHP_CodeSniffer\Files\File;
 use PHP_CodeSniffer\Sniffs\Sniff;
 
-class FunctionOrMethodParamNotationSniff implements Sniff
+class FunctionOrMethodParamCommentSniff implements Sniff
 {
     /**
      * Returns the token types that this sniff is interested in.
